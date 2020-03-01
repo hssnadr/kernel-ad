56387469a59700bba55779659bcca855e9a13680
Adrien Husson <husson.adr@gmail.com>
2020-03-01 22:17:58 +0100

    Create projects.js
    Create projects database

---
+++ store/projects.js
@@ -0,0 +1,103 @@
+export const state = () => ({
+  all: [
+    {
+      title: 'Whooshing Machine',
+      slug: 'whooshing-machine',
+      year: '2014',
+      institute: 'Strate École de Design',
+      statue: 'Student Project',
+      categories: ['Design'],
+      tags: ['interactive design', 'ux', 'ui', 'video'],
+      tools: [
+        'Adobe Illustrator',
+        'Adobe After Effects',
+        'Adobe Premiere',
+        'Adobe Photoshop'
+      ],
+      thumbnail:
+        'https://mir-s3-cdn-cf.behance.net/projects/404/06502135604629.Y3JvcCw3NjIsNTk1LDQyLDIy.jpg',
+      description: 'Diploma project at Strate École de Design'
+    },
+    {
+      title: 'Austerlitz 1805',
+      slug: 'austerlitz-1805',
+      year: '2015',
+      institute: 'Dassault Systèmes',
+      statue: 'Student Project',
+      categories: ['Design'],
+      tags: ['product design', '3d modeling', '3d rendering'],
+      tools: ['CATIA'],
+      thumbnail:
+        'https://mir-s3-cdn-cf.behance.net/projects/404/9bd43135402169.Y3JvcCw3NDgsNTg2LDMwLDIw.jpg',
+      description:
+        'Product design project made for Dassault Systèmes to highlight their 3D solution softwares.'
+    },
+    {
+      title: 'Austerlitz Standard',
+      slug: 'austerlitz-standard',
+      year: '2015',
+      institute: 'Dassault Systèmes',
+      statue: 'Student Project',
+      categories: ['Design'],
+      tags: ['product design', '3d modeling', '3d rendering'],
+      tools: ['CATIA'],
+      thumbnail:
+        'https://mir-s3-cdn-cf.behance.net/projects/404/c6c1c935401905.Y3JvcCw2NzcsNTI5LDAsMg.jpg',
+      description:
+        'Product design project made for Dassault Systèmes to highlight their 3D solution softwares.'
+    },
+    {
+      title: 'Medapli',
+      slug: 'medapli',
+      year: '2016',
+      institute: '-',
+      statue: 'Freelance Project',
+      categories: ['Design'],
+      tags: ['ux', 'ui'],
+      tools: ['Adobe Illustrator', 'Adobe Photoshop', 'Pixate Studio'],
+      thumbnail:
+        'https://mir-s3-cdn-cf.behance.net/projects/404/2e436036472477.Y3JvcCwxMDIwLDc5NywwLDA.jpg',
+      description:
+        'Medical application dedicated to senior unfamiliar with new technologies.'
+    },
+    {
+      title: 'Portail 3D',
+      slug: 'portail-3d',
+      year: '2012',
+      institute: 'Leroy Merlin',
+      statue: 'Student Project',
+      categories: ['Engineering'],
+      tags: ['programming', 'ux'],
+      tools: ['Unity 3D', 'C#'],
+      thumbnail:
+        'https://mir-s3-cdn-cf.behance.net/projects/404/22a93e11024709.Y3JvcCw0NTAsMzUyLDQxLDIw.png',
+      description: 'Online sales aid application'
+    },
+    {
+      title: 'Lumigraph',
+      slug: 'lumigraph',
+      year: '2013',
+      institute: 'Strate École de Design',
+      statue: 'Student Project',
+      categories: ['Design'],
+      tags: ['interactive design'],
+      tools: ['arduino', 'processing'],
+      thumbnail:
+        'https://mir-s3-cdn-cf.behance.net/projects/404/10904379.5480a3e764ebb.jpg',
+      description: 'My first electronics project to make light painting'
+    }
+  ]
+})
+
+// {
+//     title: '',
+//     slug: '',
+//     year: '',
+//     institute: '',
+//     statue: '',
+//     categories: [''],
+//     tags: [''],
+//     tools: [''],
+//     thumbnail: '',
+//     description: ''
+// }

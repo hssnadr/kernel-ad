ecc5365658391c579c5d0dad9fe45fd945f74823
Adrien Husson <husson.adr@gmail.com>
2020-03-04 17:19:12 +0100

    Multi categories filter

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,8 @@
       </div>
     </div>
     <h3>FILTERS</h3>
+    <!-- <button @click="setFilters()">FILTRER</button> -->
+    <p>{{ debuglog }}</p>
     <h3>MASONRY</h3>
     <no-ssr class="container-fluid">
       <div
@@ -31,14 +33,13 @@
         class="masonry-container"
         style="background-color: #f00"
       >
-        <div
-          v-for="(item, index) in projects"
+        <cardboard
+          v-for="(item, index) in filteredProjects"
           :key="index"
           v-masonry-tile
           class="item"
-        >
-          <cardboard :item="item" />
-        </div>
+          :item="item"
+        />
       </div>
     </no-ssr>
   </div>
@@ -55,30 +56,95 @@ export default {
     Logo,
     Cardboard
   },
-  // data() {
-  //   return {
-  //     statues: []
-  //   }
-  // },
+  data() {
+    return {
+      // allProjects: this.$store.state.projects.all,
+      // filtProjects: this.allProjects,
+      catFilter: [],
+      debuglog: ''
+    }
+  },
   computed: {
     projects() {
-      // return this.$store.state.projects.all
-      return this.$store.state.projects.all.filter(
-        (project) => project.statue === 'Freelance Project'
-      )
+      return this.$store.state.projects.all
+    },
+    filteredProjects() {
+      const allProj_ = this.projects
+      const categories_ = this.catFilter
+      const filtProj_ = []
+
+      for (let i = 0; i < allProj_.length; i++) {
+        // Categories
+        for (let j = 0; j < categories_.length; j++) {
+          if (allProj_[i].categories.includes(categories_[j])) {
+            filtProj_.push(allProj_[i])
+          }
+        }
+      }
+
+      return filtProj_
     }
+    // --> USE THIS TO SET FILTERS ARRAY
+    // fullName: {
+    //   // getter
+    //   get() {
+    //     return this.firstName + ' ' + this.lastName
+    //   },
+    //   // setter
+    //   set(newValue) {
+    //     const names = newValue.split(' ')
+    //     this.firstName = names[0]
+    //     this.lastName = names[names.length - 1]
+    //   }
+    // }
   },
   mounted() {
     if (typeof this.$redrawVueMasonry === 'function') {
       this.$redrawVueMasonry()
     }
   }
+  // methods: {
+  //   filteredMessages() {
+  //     console.log(this) // Our Vue component
+
+  //     const filteredMessages = this.allProjects.filter(
+  //       // References our Vue Component
+  //       (message) => this.allProjects.includes(this.match)
+  //     )
+
+  //     return filteredMessages
+  //   },
+  //   setFilters() {
+  //     console.log(this)
+
+  //     this.filtProjects = this.allProjects.filter(function(project_) {
+  //       // let isProject_ = true // boolean related to 'project_' filter (true -> project_ is return // false : not return)
+  //       // isProject_ = false
+
+  //       // this.debuglog = this.catFilter_
+  //       // const category_ = this.catFilter[0]
+  //       return project_.includes(this.catFilter[0])
+
+  //       // Filter category
+  //       if (this.catFilter.length > 0) {
+  //         let i
+  //         for (i = 0; i < this.catFilter.length; i++) {
+  //           // isProject_ = project_.categories === this.catFilter[i]
+  //           // project_.categories.components === this.catFilter[i]
+  //           return project_.includes(this.catFilter[i])
+  //         }
+  //       }
+
+  //       // return isProject_
+  //     })
+  //   }
+  // }
 }
 </script>
 
 <style>
 .item {
-  background-color: #eee;
+  /* background-color: #eee; */
   /* display: inline-block; */
   /* float: left; */
 }

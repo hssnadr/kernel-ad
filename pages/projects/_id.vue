4613b53f94c1ca7148520c2f7ee0ed53be9920a4
Adrien Husson <husson.adr@gmail.com>
2020-07-29 19:48:06 +0200

    update_test

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -39,23 +39,31 @@
     <div v-else>
       <!-- Gallery from projects/store -->
       <div v-if="isGallery">
-        <img
+        <!-- <img
           v-for="(img_, index) in project.media.images"
           :key="index"
-          src="~/assets/projects/austerlitz-1805/head.jpg"
+          :src="img_"
           class="project-pictures"
-        />
-        <!-- :src="img_" -->
+        /> -->
+
+        <div v-for="(media_, index) in project.medias" :key="index">
+          <div v-if="getImage(media_) !== null">
+            <img
+              v-if="media_.type === 'image'"
+              class="project-pictures"
+              :src="getImage(media_)"
+            />
+          </div>
+        </div>
       </div>
       <!-- iFrame from projects/store -->
       <div v-if="isIframe">
         <div v-for="(ifr_, index) in project.media.iframes" :key="index">
-          <!-- eslint-disable-next-line -->
           <p><span v-html="ifr_"></span></p>
         </div>
       </div>
-      <!-- Footer (related project) -->
     </div>
+    <!-- Footer (related project) -->
   </div>
 </template>
 
@@ -91,15 +99,22 @@ export default {
     },
     isGallery() {
       let is_ = false
-      if (Object.prototype.hasOwnProperty.call(this.project, 'media')) {
-        if (
-          Object.prototype.hasOwnProperty.call(this.project.media, 'images')
-        ) {
-          if (this.project.media.images != null) {
-            is_ = this.project.media.images.length > 0
-          }
+      // if (Object.prototype.hasOwnProperty.call(this.project, 'media')) {
+      //   if (
+      //     Object.prototype.hasOwnProperty.call(this.project.media, 'images')
+      //   ) {
+      //     if (this.project.media.images != null) {
+      //       is_ = this.project.media.images.length > 0
+      //     }
+      //   }
+      // }
+
+      if (Object.prototype.hasOwnProperty.call(this.project, 'medias')) {
+        if (this.project.medias != null) {
+          is_ = this.project.medias.length > 0
         }
       }
+
       return is_
     },
     isIframe() {
@@ -136,6 +151,20 @@ export default {
       if (link_ !== null) {
         window.open(link_, '_blank')
       }
+    },
+    getImage(media_) {
+      let link_ = null
+      if (media_.source === 'external') {
+        // Improvement: check if link is working (else return null)
+        link_ = media_.link
+      } else {
+        try {
+          link_ = require('~/' + media_.source + '/' + media_.link)
+        } catch (err) {
+          link_ = null
+        }
+      }
+      return link_
     }
   }
 }

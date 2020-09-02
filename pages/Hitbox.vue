191f70000fb1030be77e8613fb08a5d72c2dacbf
Adrien Husson <husson.adr@gmail.com>
2020-09-02 13:46:33 +0200

    hitbox logo anim

--- pages/Hitbox.vue
+++ pages/Hitbox.vue
@@ -1,8 +1,33 @@
 <template>
-  <div>
-    <!-- <header id="hitbox-header"></header> -->
-    <div class="hitbox-background1"></div>
-    <!-- style="background-attachment: scroll;" -->
+  <div id="hitbox-page">
+    <header class="hitbox-background1">
+      <hitbox-logo ref="hbxlogo" class="hitbox-logo" />
+    </header>
+
+    <div class="overview justify-center">
+      <cri-logo class="cri-logo" />
+      <p>
+        Hitbox was developed at the Center for Research and Interdisciplinarity
+        (CRI) based in Paris. This laboratory experiments and spreads new ways
+        of learning, teaching, conducting research and mobilizing collective
+        intelligence in life, learning and digital sciences.
+      </p>
+      <p>
+        With Hitbox, the former purpose is to explore ways to interact with
+        technologies in an education process involving the body. Connecting
+        several Hitbox together allows to create innovative boxing scenarios
+        where beginners interact with each others without risk of injuries. The
+        integrated and interactive screen drive their learning through specific
+        scenarios addressing specific aspect of boxing: movement, reactivity,
+        strength…
+      </p>
+      <p>
+        Based on a crossed approach research / design / engineering, Hitbox is
+        now dedicated to explore new interaction by developing various immersive
+        and tangible digital spaces for any kind of application: health,
+        communication, events…
+      </p>
+    </div>
 
     <!-- VIDEO -->
     <div class="iframe-container">
@@ -29,37 +54,99 @@
 </template>
 
 <script>
+import HitboxLogo from '~/components/hitbox/hitbox-logo.vue'
+import CriLogo from '~/components/motionlab/cri-logo.vue'
+
 export default {
+  components: {
+    HitboxLogo,
+    CriLogo
+  },
+  data() {
+    return {
+      windowHeight: 0,
+      scrollY: 0
+    }
+  },
   mounted() {
     // Hide side bar
     this.$store.commit('navigator/SET_COLORSWAP', true)
     this.$store.commit('sidemenu/SetState', false)
     this.$store.commit('sidemenu/HIDE_SIDEBAR', true)
+
+    // Window resize listener
+    this.$nextTick(function() {
+      window.addEventListener('resize', this.windowResize)
+      this.windowResize()
+    })
+
+    // Scroll listener
+    this.$nextTick(function() {
+      window.addEventListener('scroll', this.scrollEvent)
+      this.scrollEvent()
+    })
+  },
+  methods: {
+    scrollEvent(event) {
+      this.scrollY = window.scrollY
+
+      let viewOnHeader_ = this.scrollY / this.windowHeight
+      if (Math.abs(viewOnHeader_) > 1) {
+        viewOnHeader_ = 1
+      }
+
+      this.$refs.hbxlogo.setAnimProg((1 - viewOnHeader_) ** 5)
+    },
+    windowResize(event) {
+      this.windowHeight = document.documentElement.clientHeight
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#hitbox-header {
-  background-image: url('~assets/hitbox/pics/Hitbox1.jpg');
-  background-attachment: fixed;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
+#hitbox-page {
+  header {
+    background-image: url('~assets/hitbox/pics/Hitbox1.jpg');
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 
-  height: 100vh;
-  width: 100%;
+    height: 100vh;
+    width: 100%;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: $base-color;
-}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: $base-color;
 
-@for $i from 1 through 5 {
-  .hitbox-background#{$i} {
-    @extend #hitbox-header;
-    background-image: url('~assets/hitbox/pics/Hitbox#{$i}.jpg');
+    .hitbox-logo {
+      width: 60%;
+    }
+  }
+
+  @for $i from 1 through 5 {
+    .hitbox-background#{$i} {
+      @extend header;
+      background-image: url('~assets/hitbox/pics/Hitbox#{$i}.jpg');
+    }
+  }
+
+  .overview {
+    width: 50%;
+    margin: 0 auto;
+    padding: 50px 0;
+
+    .cri-logo {
+      display: block;
+      width: 400px;
+      margin: 0 auto 2rem auto;
+    }
+
+    p {
+      margin-bottom: 2rem;
+    }
   }
 }
 </style>
